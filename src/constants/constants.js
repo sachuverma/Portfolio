ca5bd68a6450db04c6440a91e839749ac9a9ba65
Sachin Verma <sachinverma53121@gmail.com>
2021-08-14 16:27:03 +0530

    content

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -19,7 +19,7 @@ export const projects = [
   },
   {
     title: 'Algorithms Visualiser',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    description: "This project is make using HTML, CSS, and Javascript visualising different algorithms. Graph, Breadth First Search and Deapth First Search algos are shown using a grid matrix. Visualisation of different sorting algo like Merge Sort, Buble Sort, Insertion Sort, Radix SOrt are also implemented.",
     image: '/images/3-min.jpg',
     tags: ['HTML', 'CSS', 'JavaScript' , 'Data Structures'],
     source: 'https://github.com/sachuverma/Algorithms-Visualization',
@@ -28,9 +28,9 @@ export const projects = [
   },
   {
     title: 'All Clones',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    description: "This is a code repository containing many clones. First Clone is Spotify Clone used Spotify API for user authentication, and fetching user playlists and Weekly Playlists details. Second clone is Youtube Front-end Clone having youtube home page, with videos list, youtube searchbar, and search page showing channel details and videos list. Thirs clone is Google Drive clone with auth, and upload functionalities (work in progress).",
       image: '/images/4-min.jpg',
-      tags: ['React', 'Firebase'],
+      tags: ['React', 'Firebase', 'APIs'],
     source: 'https://github.com/sachuverma/All-Clones',
     visit: 'https://github.com/sachuverma/All-Clones/blob/master/README.md',
     id: 3,
