deb1ec1ad3e16330e3940c3cfb2a0ca7dc414232
Sachin Verma <sachinverma53121@gmail.com>
2021-08-22 01:54:18 +0530

    new changes

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -13,7 +13,23 @@ const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
     scroll-behavior: smooth;
 
+    ::-webkit-scrollbar {
+      width: 6px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: rgba(104,121,209,0.2);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: linear-gradient(to bottom, rgba(147,94,214,0.6), rgba(21,172,200,0.6));
+    }
+    
+    ::-webkit-scrollbar-thumb:hover {
+      background: linear-gradient(to bottom, #915FD6, #17AAC7);
+    }
   }
+
   body {
     font-family: ${props => props.theme.fonts.main};
     font-size: 1.6rem;
@@ -22,12 +38,15 @@ const GlobalStyles = createGlobalStyle`
     cursor: default;
 
   }
+
   h1,h2,h3,h4,h5,h6,button {
     font-family: ${props => props.theme.fonts.title};
   }
+
   a {
     text-decoration: none;
   }
+  
   li{
     list-style: none;
   }
